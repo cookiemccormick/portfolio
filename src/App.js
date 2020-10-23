8e608169a55ad36a3146b5e1ae2bb79fe4aa0e55
cookiemccormick <alisa.cookie.mccormick@gmail.com>
2020-10-22 23:22:39 -0400

    updated Nav

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Navbar.Brand href="/">Alisa Cookie McCormick</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>
